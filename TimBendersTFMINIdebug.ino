ef57a1e60a758485659f10c87c5a8677a2f0755f
Dorsel89 <d.dorsel@gmx.de>
2020-05-25 15:45:50 +0200

    Update TimBendersTFMINIdebug.ino

--- TimBendersTFMINIdebug.ino
+++ TimBendersTFMINIdebug.ino
@@ -1,32 +1,19 @@
 #include <phyphoxBle.h>
-#include "TFmini.h"
+#include "TFMini.h"
 
-TFmini tfmini;
+TFMini tfmini;
 BleServer server("Arduino");
 void setup()
 {
     Serial.begin(115200);
     delay(2000);
+    Serial1.begin(115200);
+    delay(2000);
     server.start();
     Serial1.begin(TFmini::DEFAULT_BAUDRATE);
-    tfmini.attach(Serial1);
+    tfmini.begin(&Serial1);    
     delay(1000);
 
-    const TFmini::DetectionPattern myPattern = TFmini::DetectionPattern::Fixed;
-    tfmini.setDetectionPattern(myPattern);
-    delay(1000);
-    while (Serial1.available()) {
-      Serial.write(Serial1.read());
-    }
-
-    const TFmini::DistanceMode myMode = TFmini::DistanceMode::Long;
-
-    tfmini.setDistanceMode(myMode);
-    
-    while (Serial1.available()) {
-      Serial.write(Serial1.read());
-    }
-    delay(2000);
 }
 
 void loop()
@@ -35,9 +22,9 @@ void loop()
     {
         float distance, strength, integrationTime, timestamp;
         timestamp = millis()/1000.0;
-        distance = tfmini.getDistance();
-        strength = tfmini.getStrength();
-        integrationTime = tfmini.getIntegrationTime();
+        distance = dist = tfmini.getDistance();
+        strength = tfmini.getRecentSignalStrength();
+        integrationTime = 1.;//tfmini.getIntegrationTime();
         Serial.println(distance);
         delay(30);
         server.write(distance,strength,timestamp, integrationTime);
